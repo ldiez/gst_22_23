17a95966b1dcab14195f9099bb8c5b6bc3dc83db
Luis Diez <ldiez@tlmat.unican.es>
2022-03-19 13:01:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main () {
-  printf ("hello world\n");
-  
-  
+int Func1(int i)
+{
+  return i != 8 ? i : -1;
+}
+
+int main()
+{
+  int ret = 0;
+  for (int i = 0; i < 10; ++i)
+  {
+    if (Func1(i) == i)
+    {
+      printf("**** PASS: Func1 with input %d\n", i);
+    }
+    else
+    {
+      printf("**** FAILED: Func1 with input %d\n", i);
+      int ret = -1;
+    }
+  }
+  return ret;
 }
