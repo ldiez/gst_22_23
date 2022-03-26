e7797986d6988f9e93987f4584296106512e1cef
ldiez <ldiez@tlmat.unican.es>
2022-03-26 17:55:32 +0100

    ++

--- main.c
+++ main.c
@@ -3,28 +3,42 @@
 #include <assert.h>
 #include <stdbool.h>
 
-int Func1(int i)
+int Basic(int i)
 {
   // return i != 6 ? i : -1;
   return i;
+}
+
+int Feature1(int i)
+{
+  return 2*i;
 }
 
 
 int main()
 {
   int ret = 0;
-  for (int i = 0; i < 10; ++i)
-  {
-    if (Func1(i) == i)
+  for (int i = 1; i <= 10; ++i){
+
+    // Test basic functionality
+    if (Basic(i) == i)
     {
       printf("**** PASS: Func1 with input %d\n", i);
     }
     else
     {
       printf("**** FAILED: Func1 with input %d\n", i);
-      // assert(false && "**** FAILED: Func1 with input %d\n");
+      ret = -1;
+    }
+
+    // Test feature1
+    if (Feature1(i) == 2*i){
+      printf("**** PASS: Feature1 with input %d\n", i);
+    } else {
+      printf("**** FAILED: Feature1 with input %d\n", i);
       ret = -1;
     }
   }
+
   return ret;
 }
